cdb886b6ec39bc5ca18e04cba30d379a11f3cf1c
joreikarr <joreikarr@gmail.com>
2024-12-06 16:08:52 +0200

    Add Circle Area Calculator

---
+++ app/src/main/java/ua/joreikarr/geometrycalculator/CircleAreaCalculator.java
@@ -0,0 +1,7 @@
+package ua.joreikarr.geometrycalculator;
+
+public class CircleAreaCalculator {
+    public static double calculateArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+}
--- app/src/main/java/ua/joreikarr/geometrycalculator/MainActivity.java
+++ app/src/main/java/ua/joreikarr/geometrycalculator/MainActivity.java
@@ -1,14 +1,35 @@
 package ua.joreikarr.geometrycalculator;
 
-package com.example.geometrycalculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText radiusInput = findViewById(R.id.radiusInput);
+        TextView resultView = findViewById(R.id.resultView);
+        Button calculateButton = findViewById(R.id.calculateButton);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String radiusText = radiusInput.getText().toString();
+                if (!radiusText.isEmpty()) {
+                    double radius = Double.parseDouble(radiusText);
+                    double area = CircleAreaCalculator.calculateArea(radius);
+                    resultView.setText(String.format("Area: %.2f", area));
+                } else {
+                    resultView.setText("Please enter a radius");
+                }
+            }
+        });
     }
 }
