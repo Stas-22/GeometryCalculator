542700f81765998cb97dcc95ebfa42eb0bd10a14
joreikarr <joreikarr@gmail.com>
2024-12-06 16:05:31 +0200

    Initial commit

---
+++ app/src/main/java/ua/joreikarr/geometrycalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package ua.joreikarr.geometrycalculator;
+
+package com.example.geometrycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
